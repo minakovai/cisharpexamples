0feea356dc9b4cd5b48cda64d3b1c5072e601aa4
Aleksandr Minakov <am@infodrive.pro>
2023-07-31 09:25:05 +0300

    Add example with else

---
+++ Example05ELSE/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Please input user name ");
+string username = Console.ReadLine();
+if (username.ToLower() == "mary")
+{
+    Console.WriteLine ("Yehoo! This is Mary!");
+}
+else
+{
+    Console.Write ("Hello, ");
+    Console.WriteLine (username);
+}
