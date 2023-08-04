fb6ed6df191bb2a23d0d66bc2bbc9867fa3df073
Aleksandr Minakov <am@infodrive.pro>
2023-08-04 18:08:21 +0300

    The program work correctly

--- Example07Magic/Program.cs
+++ Example07Magic/Program.cs
@@ -1,6 +1,7 @@
 ﻿Console.Clear ();
-Console.SetCursorPosition(10,4);
-Console.WriteLine("+");
+//Console.SetWindowSize(100,100);
+//Console.SetCursorPosition(10,4);
+//Console.WriteLine("+");
 
 int xa = 40, ya = 1, 
     xb = 1, yb = 30,
